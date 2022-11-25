7691ce50fe6877c49206625e832a2e1cc24a140c
Basit Malik <mbasit467@gmail.com>
2022-11-25 23:05:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <App/>
+  </BrowserRouter>
 );
 
 
