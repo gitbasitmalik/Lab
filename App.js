dba692b48ce319f17c5b50fafb37560341c0e3d7
Basit Malik <mbasit467@gmail.com>
2022-11-25 23:05:07 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-
+import { Routes , Route } from "react-router-dom";
 import Main from "./Main";
+import QAETrophy from "./QAETrophy";
+import IosApp from "./IosApp";
 
 function App() 
 {
   return ( 
     <div>
-     
+     <Routes>
+      <Route path="/QAETrophy" element={<QAETrophy/>}/>
+      <Route path="/IosApp" element={<IosApp/>}/>
+     </Routes>
       <Main/>
     </div>
    );
